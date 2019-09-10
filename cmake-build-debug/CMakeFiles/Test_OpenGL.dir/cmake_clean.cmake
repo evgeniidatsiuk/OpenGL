b8927cd911db2420618efc162957e29cf2247a44
evgeniidatsiuk <evgenii.datsiuk@gmail.com>
2019-09-10 12:40:58 +0300

    Hello World!

---
+++ cmake-build-debug/CMakeFiles/Test_OpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test_OpenGL.dir/main.cpp.o"
+  "Test_OpenGL.pdb"
+  "Test_OpenGL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test_OpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
