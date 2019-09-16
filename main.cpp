00696402948c717559eab0bd0f8c9cca981ca14f
evgeniidatsiuk <evgenii.datsiuk@gmail.com>
2019-09-16 11:26:45 +0300

    Labolatory work 1

--- main.cpp
+++ main.cpp
@@ -1,103 +1,70 @@
-#include <stdlib.h>
 #include <GL/glut.h>
-#include <iostream>
 
-using namespace std;
+void display(void)
+{
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
-// инициализация переменных цвета в 1.0
-// треугольник - белый
-float red=1.0f, blue=1.0f, green=1.0f;
-
-// угол поворота
-float angle = 0.0f;
-
-void changeSize(int w, int h) {
-    // предотвращение деления на ноль
-    if (h == 0)
-        h = 1;
-    float ratio =  w * 1.0 / h;
-    // используем матрицу проекции
-    glMatrixMode(GL_PROJECTION);
-    // обнуляем матрицу
-    glLoadIdentity();
-    // установить параметры вьюпорта
-    glViewport(0, 0, w, h);
-    // установить корректную перспективу
-    gluPerspective(45.0f, ratio, 0.1f, 100.0f);
-    // вернуться к матрице проекции
-    glMatrixMode(GL_MODELVIEW);
-}
-
-void renderScene(void) {
-
-    // очистить буфер цвета и глубины.
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    // обнулить трансформацию
-    glLoadIdentity();
-    // установить камеру
-    gluLookAt( 0.0f, 0.0f, 10.0f,
-               0.0f, 0.0f,  0.0f,
-               0.0f, 1.0f,  0.0f);
-    //поворот на заданную величину
-    glRotatef(angle, 0.0f, 1.0f, 0.0f);
-    // установить цвет модели
-    glColor3f(red,green,blue);
+    // трикутник
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+//    glBegin(GL_TRIANGLES);
+//    glColor3f(1.0,1.0,1.0);
+//    glVertex3f(-0.25,0.0,0.0);
+//    glVertex3f(0.0,0.75,0.0);
+//    glVertex3f(0.25,0.0,0.0);
+//    glEnd();
     glBegin(GL_LINE_LOOP);
-    glVertex3f(-2.0f,-2.0f, 0.0f);
-    glVertex3f( 0.0f, 2.0f, 0.0);
-    glVertex3f( 2.0f,-2.0f, 0.0);
+    glVertex3f(-0.25,0.0,0.0);
+    glVertex3f(0.0,0.75,0.0);
+    glVertex3f(0.25,0.0,0.0);
     glEnd();
 
-    angle+=0.1f;
 
-    glutSwapBuffers();
+    // лінія
+    glLineWidth(2.0);
+    glEnable(GL_LINE_STIPPLE);
+    glLineStipple(1,0xAAAA);
+    glBegin(GL_LINES);
+    glVertex2f(-1.50,0.8);
+    glVertex2f(1.50,0.8);
+    glEnd();
+    glDisable(GL_LINE_STIPPLE);
+
+    // точки
+    glPointSize(12);
+    glBegin(GL_POINTS);
+    glColor3f(1.0,1.0,1.0);
+    glVertex3f(-0.25,0.6,0.0);
+    glColor3f(0.0,1.0,1.0);
+    glVertex3f(0.0,0.75,0.0);
+    glColor3f(0.0,0.0,1.0);
+    glVertex3f(0.25,0.6,0.0);
+    glEnd();
+
+
+    // "трапеція"
+    glLineWidth(2.0);
+    glEnable(GL_LINE_STIPPLE);
+    glLineStipple(1,0x00FF);
+    glBegin(GL_QUADS);
+
+    glColor3f(0.0,1.0,0.0);
+    glVertex2f(0.25f, 0.0f);
+    glVertex2f(0.5f, -1.0f);
+    glVertex2f(-0.5f, -1.0f);
+    glVertex2f(-0.25f, 0.0f);
+
+    glEnd();
+    glFlush();
 }
 
-void processNormalKeys(unsigned char key, int x, int y) {
-
-    if (key == 27)
-        exit(0);
-}
-
-void processSpecialKeys(int key, int x, int y) {
-
-    switch(key) {
-        case GLUT_KEY_F1 :
-            red = 1.0;
-            green = 0.0;
-            blue = 0.0; break;
-        case GLUT_KEY_F2 :
-            red = 0.0;
-            green = 1.0;
-            blue = 0.0; break;
-        case GLUT_KEY_F3 :
-            red = 0.0;
-            green = 0.0;
-            blue = 1.0; break;
-    }
-}
-
-
-
-int main(int argc, char **argv) {
-
-    // инициализация
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+int main(int argc, char **argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutInitWindowSize(700,700);
     glutInitWindowPosition(100,100);
-    glutInitWindowSize(400,400);
-    glutCreateWindow("4");
-    // регистрация
-    glutDisplayFunc(renderScene);
-    glutReshapeFunc(changeSize);
-    glutIdleFunc(renderScene);
-
-    // наши новые функции
-    glutKeyboardFunc(processNormalKeys);
-    glutSpecialFunc(processSpecialKeys);
-
-    // основной цикл
+    glutCreateWindow("EUGENE");
+    glutDisplayFunc(display);
     glutMainLoop();
-    cout << "lol";
-    return 1;
+    return 0;
 }
